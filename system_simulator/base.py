55390136eef1d1068c9de2253d8c819804f7f466
zwang <zwang@stu.xmu.edu.cn>
2023-01-15 07:50:01 +0800

    fix the bugs of time jumping in sample that is conflict to the communicate_with_clock

--- system_simulator/base.py
+++ system_simulator/base.py
@@ -264,9 +264,9 @@ def with_availability(sample):
     def sample_with_availability(self):
         available_clients = cfg.state_updater.idle_clients
         # ensure that there is at least one client to be available at the current moment
-        while len(available_clients) == 0:
-            cfg.clock.step()
-            available_clients = cfg.state_updater.idle_clients
+        # while len(available_clients) == 0:
+        #     cfg.clock.step()
+        #     available_clients = cfg.state_updater.idle_clients
         # call the original sampling function
         selected_clients = sample(self)
         # filter the selected but unavailable clients
